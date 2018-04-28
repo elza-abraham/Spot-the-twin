f56d69cefd7b40358e645c4ed5dd86f021e5654c
elza-abraham <elzaeltho@gmail.com>
2018-04-27 23:23:18 -0700

    route added

--- app/index.js
+++ app/index.js
@@ -1,14 +1,3 @@
-// import React from 'react'
-// import ReactDOM from 'react-dom'
-// import {Route} from 'react-router-dom'
-// import MainPage from './MainPage'
-
-// ReactDOM.render(
-//   <MainPage />,
-//   document.getElementById('app')
-// )
-
-
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Main from './Main'
